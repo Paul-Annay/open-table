a09df6f764d7200f5bff7f5f2fbcc77cf8e28d0b
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-07-31 19:21:16 +0600

    * Data-Handling: In the app/restaurant/[slug]/menu/page.tsx created the fetchRestaurantMenu function which gets the menu specific to the slug of a restaurant. * Modifications: Updated the Menu and MenuCard components within the app/restaurant/[slug]/components to reflect dynamic data changes.

--- app/restaurant/[slug]/components/Menu.tsx
+++ app/restaurant/[slug]/components/Menu.tsx
@@ -1,15 +1,24 @@
+import { Item } from "@prisma/client";
 import MenuCard from "./MenuCard";
 
-function Menu() {
+function Menu({ menu }: { menu: Item[] }) {
     return (
         <main className='bg-white mt-5'>
             <div>
                 <div className='mt-4 pb-1 mb-1'>
                     <h1 className='font-bold text-4xl'>Menu</h1>
                 </div>
-                <div className='flex flex-wrap justify-between'>
-                    <MenuCard />
-                </div>
+                {menu.length ? (
+                    <div className='flex flex-wrap justify-between'>
+                        {menu.map((item, i) => (
+                            <MenuCard key={i} item={item} />
+                        ))}
+                    </div>
+                ) : (
+                    <div className='flex flex-wrap justify-between'>
+                        <p>This restaurant does not have a menu to display</p>
+                    </div>
+                )}
             </div>
         </main>
     );
