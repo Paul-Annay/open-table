451625dae099407006b38be2be51e5bcd730afa4
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-08-01 19:51:21 +0600

    * Logic: Created the /utils/calculateReviewRatingAverage.ts file for calculating ratings from reviews. * Modifications: - Updated the RestaurantCard in /app/components for dynamic data changes - Created the ReviewCard.tsx component and updated the Rating.tsx and Review.tsx from /app/restaurant/[slug]/components for dynamic data-changes. - Updated the RestaurantCard.tsx from the /app/search/components to reflect dynamic data changes. * Data-Handling: Updated the /app/page.tsx, /app/search/page.tsx & /app/restaurant/[slug]/page.tsx to fetch reviews data and pass them to the relevant components.

--- app/search/components/RestaurantCard.tsx
+++ app/search/components/RestaurantCard.tsx
@@ -1,6 +1,7 @@
-import { Cuisine, Location, PRICE } from "@prisma/client";
+import { Cuisine, Location, PRICE, Review } from "@prisma/client";
 import Link from "next/link";
 import Price from "../../components/Price";
+import { calculateReviewRatingAverage } from "../../../utils/calculateReviewRatingAverage";
 
 interface Restaurant {
     id: number;
@@ -10,9 +11,18 @@ interface Restaurant {
     price: PRICE;
     cuisine: Cuisine;
     slug: string;
+    reviews: Review[];
 }
 
 function RestaurantCard({ restaurant }: { restaurant: Restaurant }) {
+    const renderRatingText = () => {
+        const rating = calculateReviewRatingAverage(restaurant.reviews);
+
+        if (rating > 4) return "Awesome";
+        else if (rating <= 4 && rating > 3) return "Good";
+        else if (rating <= 3 && rating > 0) return "Average";
+        else return "";
+    };
     return (
         <div className='border-b flex pb-5 ml-4'>
             <img
@@ -24,7 +34,7 @@ function RestaurantCard({ restaurant }: { restaurant: Restaurant }) {
                 <h2 className='text-3xl'>{restaurant.name}</h2>
                 <div className='flex items-start'>
                     <div className='flex mb-2'>*****</div>
-                    <p className='ml-2 text-sm'>Awesome</p>
+                    <p className='ml-2 text-sm'>{renderRatingText()}</p>
                 </div>
                 <div className='mb-9'>
                     <div className='font-light flex text-reg'>
