f7e8b50d7b2552bc6e90338c8e1cf831e759821c
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-10-07 15:39:59 +0600

    * Feature: Validating user input. * Set-up: Installed validator js. * Logic: Implemented a validationSchema in /page/api/auth/signup.tsx that validates user input and provides error messages accordingly.

--- pages/api/auth/signup.ts
+++ pages/api/auth/signup.ts
@@ -1,12 +1,61 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
+import validator from "validator";
 export default async function handler(
     req: NextApiRequest,
     res: NextApiResponse
 ) {
     if (req.method === "POST") {
+        const { firstName, lastName, email, phone, city, password } = req.body;
+        const errors: string[] = [];
+
+        const validationSchema = [
+            {
+                valid: validator.isLength(firstName, {
+                    min: 1,
+                    max: 20,
+                }),
+                errorMessage: "First Name is Invalid",
+            },
+            {
+                valid: validator.isLength(lastName, {
+                    min: 1,
+                    max: 20,
+                }),
+                errorMessage: "Last Name is Invalid",
+            },
+            {
+                valid: validator.isEmail(email),
+                errorMessage: "Email is Invalid",
+            },
+            {
+                valid: validator.isMobilePhone(phone),
+                errorMessage: "Phone number is Invalid",
+            },
+            {
+                valid: validator.isStrongPassword(password),
+                errorMessage: "Password is Invalid",
+            },
+            {
+                valid: validator.isLength(city, {
+                    min: 1,
+                }),
+                errorMessage: "City  is Invalid",
+            },
+        ];
+
+        validationSchema.forEach((check) => {
+            if (!check.valid) {
+                errors.push(check.errorMessage);
+            }
+        });
+
+        if (errors.length) {
+            return res.status(400).json({
+                errorMessage: errors[0],
+            });
+        }
         res.status(200).json({
-            hello: "there",
+            hello: "There",
         });
     }
 }
