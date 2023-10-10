e31b4cc6dad6b72afca5bad95b512c9afa8a5777
Annay Paul <annay.paul@g.bracu.ac.bd>
2023-10-11 00:15:36 +0600

    * Feature: Created middleware for handling token validation and verification logic. * Modified: Extracted the token and bearerToken logic from /pages/api/auth/me.ts. * Logic: -Created /middleware.ts Implemented the previously introduced token existence and verification logic.

--- pages/api/auth/me.ts
+++ pages/api/auth/me.ts
@@ -1,5 +1,4 @@
 import { NextApiRequest, NextApiResponse } from "next";
-import * as jose from "jose";
 import jwt from "jsonwebtoken";
 import { PrismaClient } from "@prisma/client";
 
@@ -10,31 +9,7 @@ export default async function handler(
     res: NextApiResponse
 ) {
     const bearerToken = req.headers["authorization"] as string;
-
-    if (!bearerToken) {
-        res.status(401).json({
-            errorMessage: "Unauthorized Request (no bearer token)",
-        });
-    }
-
     const token = bearerToken.split(" ")[1];
-
-    if (!token) {
-        res.status(401).json({
-            errorMessage: "Unauthorized Request (no token)",
-        });
-    }
-
-    const secret = new TextEncoder().encode(process.env.JWT_SECRET);
-
-    try {
-        await jose.jwtVerify(token, secret);
-    } catch (error) {
-        return res.status(401).json({
-            errorMessage: "Unauthorized Request (invalid token)",
-        });
-    }
-
     const payload = jwt.decode(token) as { email: string };
 
     if (!payload.email) {
